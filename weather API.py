cfabedb947d8ff05575018d54a370a6ad30323b5
joodmicmuffin-bit <amjad.alolyan@gmail.com>
2026-04-20 12:34:05 +0100

    added UI styling and CSS for weather app

--- weather API.py
+++ weather API.py
@@ -1,4 +1,3 @@
-
 import sys
 import requests
 
@@ -11,34 +10,94 @@ from PyQt5.QtWidgets import (
     QVBoxLayout
 )
 
-
 from PyQt5.QtCore import Qt
+
 
 class WeatherApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.city_label: QLabel = ("City Name ", self)
-        self.input =QLineEdit(self)
+
+        self.city_label = QLabel("City Name", self)
+        self.city_input = QLineEdit(self)
         self.get_weather_button = QPushButton("Get Weather", self)
-        self.Temperature_label: QLabel("Temperature ", self)
-        self.emoji_label: QLabel("Emoji ", self)
-        self.description_label = QLabel("Description ", self)
+        self.temperature_label = QLabel("30°C", self)
+        self.emoji_label = QLabel("🌞", self)
+        self.description_label = QLabel("Description", self)
+
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Weather API")
+        self.setGeometry(300, 300, 350, 500)
+
+        vbox = QVBoxLayout()
+
+
+
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
+
+
+        self.setLayout(vbox)
+
+        self.city_label.setAlignment(Qt.AlignCenter)
+        self.city_input.setAlignment(Qt.AlignCenter)
+        self.temperature_label.setAlignment(Qt.AlignCenter)
+        self.emoji_label.setAlignment(Qt.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignCenter)
+
+        # set objects ---->
+
+        self.city_label.setObjectName('city_label')
+        self.city_input.setObjectName('city_input')
+        self.temperature_label.setObjectName('temperature_label')
+        self.emoji_label.setObjectName('emoji_label')
+        self.description_label.setObjectName('description_label')
+        self.get_weather_button.setObjectName('get_weather_button')
+
+
+        #CSS code part
+
+        self.setStyleSheet("""
+            QLabel,QPushButton {
+                font-family: calibri;
+            }
+
+            QLabel#city_label {
+                font-size: 25px;
+            }
+            
+            QLineEdit#city_input {
+                font-size: 20px;
+            }
+            
+            QPushButton#get_weather_button {
+                font-size: 25px;
+                font-weight: bold;
+            }
+            
+            QLabel#temperature_label {
+                font-size: 60px;
+            }
+            
+            QLabel#emoji_label {
+                font-size: 70px;
+            }
+            
+            QLabel#description_label {
+                font-size: 40px; 
+            }
+            
+            
+        """)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    Weather_App = WeatherApp()
-    Weather_App.show()
+    weather_app = WeatherApp()
+    weather_app.show()
     sys.exit(app.exec_())
-
-
-
-
-
-
-
-
-
-
-
-
